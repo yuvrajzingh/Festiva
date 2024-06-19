a612f8be8c9abbd15b6cd8b30c7c57340af1324e
Yuvraj Singh <yuvisingh1810@gmail.com>
2024-06-19 23:35:26 +0530

    Footer && home page && mobilenav

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,23 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='border-5'>
+      <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
+        <Link href='/'>
+          <Image 
+            src="/assets/images/logo.png"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <p>2024 Festiva. All Rigts reserved.</p>
+      </div>
+    </footer>
   )
 }
 
